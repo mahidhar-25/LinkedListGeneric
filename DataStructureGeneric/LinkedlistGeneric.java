61bfdf502426c315061fac89793d274c1830f095
mahidhar reddy <mahidhar100008@gmail.com>
2023-11-27 19:20:51 +0530

    Added : initialize the linked list generic problem

---
+++ DataStructureGeneric/LinkedlistGeneric.java
@@ -0,0 +1,9 @@
+package DataStructureGeneric;
+
+
+public class LinkedlistGeneric<T> {
+    public static void main(String[] args) {
+        System.out.println("!!! Welcome the linked list generic problem !!!");
+    }
+
+}
